d208cd87f582680cc4efb3c2b5e0f7e7adb31f36
boboxiaodd <iam386@gmail.com>
2023-11-11 11:56:26 +0800

    Update Pinger.js

--- src/Pinger.js
+++ src/Pinger.js
@@ -11,33 +11,39 @@ import ClientImplementation from './ClientImplementation';
 export default class {
   _client: ClientImplementation;
   _keepAliveIntervalMs: number;
+  _runAfter: Function;
   pingReq: ArrayBuffer = new WireMessage(MESSAGE_TYPE.PINGREQ).encode();
   timeout: ?number;
 
-  constructor(client: ClientImplementation, keepAliveIntervalSeconds: number) {
+  constructor(client: ClientImplementation, keepAliveIntervalSeconds: number , runAfter: Function) {
     this._client = client;
     this._keepAliveIntervalMs = keepAliveIntervalSeconds * 1000;
+    this._runAfter = runAfter ? runAfter : setTimeout;
     this.reset();
   }
 
   _doPing() {
     this._client._trace('Pinger.doPing', 'send PINGREQ');
-    this._client.socket && this._client.socket.send(this.pingReq);
-    this.timeout = setTimeout(() => this._doPing(), this._keepAliveIntervalMs);
+    if(this._client.socket) {
+      this._client.socket.send(this.pingReq);
+      this._runAfter(() => this._doPing(), this._keepAliveIntervalMs);
+    }else{
+      this._client._trace('Pinger.doPing', 'socket closed');
+    }
   }
 
   reset() {
-    if (this.timeout) {
-      clearTimeout(this.timeout);
-      this.timeout = null;
-    }
+    // if (this.timeout) {
+      // clearTimeout(this.timeout);
+      // this.timeout = null;
+    // }
     if (this._keepAliveIntervalMs > 0) {
-      this.timeout = setTimeout(() => this._doPing(), this._keepAliveIntervalMs);
+      this._runAfter(() => this._doPing(), this._keepAliveIntervalMs);
     }
   }
 
   cancel() {
-    clearTimeout(this.timeout);
-    this.timeout = null;
+    // clearTimeout(this.timeout);
+    // this.timeout = null;
   }
 }
